ec9ee8100251b1d48a03f836e5939010aed3a61b
Dave Conroy <dconroy@gmail.com>
2013-10-29 15:51:03 -0400

    Create stt.sh
    This script by default only does Speech to text.
    Uncomment the second line if you want to enable the translation

---
+++ stt.sh
@@ -0,0 +1,16 @@
+#DaveConroy.com
+#10/29/13
+#stt.sh
+
+arecord -D plughw:0,0 -f cd -t wav -d 0 -r 16000 | flac - -f --best --sample-rate 16000 -o daveconroy.flac;
+
+echo "Converting Speech to Text..."
+wget -q -U "Mozilla/5.0" --post-file daveconroy.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  > stt.txt
+
+echo "You Said:"
+value=`cat stt.txt`
+echo "$value"
+
+#part 2
+#python pitranslate.py -o en -d es -t "$value"
+
