ec5c199f898ceeeebfeadbb77d363176caa64fab
Dave Conroy <dconroy@gmail.com>
2013-10-29 20:52:53 -0400

    Update stt.sh
    added screen prompts and ran arecord and flac in silent mode

--- stt.sh
+++ stt.sh
@@ -2,7 +2,8 @@
 #10/29/13
 #stt.sh
 
-arecord -D plughw:0,0 -f cd -t wav -d 0 -r 16000 | flac - -f --best --sample-rate 16000 -o daveconroy.flac;
+echo "Recording your Speech (Ctrl+C to Transcribe)"
+arecord -D plughw:0,0 -f cd -t wav -d 0 -q -r 16000 | flac - -s -f --best --sample-rate 16000 -o daveconroy.flac;
 
 echo "Converting Speech to Text..."
 wget -q -U "Mozilla/5.0" --post-file daveconroy.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  > stt.txt
