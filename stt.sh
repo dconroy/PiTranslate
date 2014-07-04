ad16a936cb40c5c614f87c078bdcdf7b8fb85c58
Dave Conroy <dconroy@gmail.com>
2014-07-04 17:54:33 -0400

    Update stt.sh
    because of google speech api v2, i broke this into 3 files

--- stt.sh
+++ stt.sh
@@ -1,17 +1,13 @@
 #DaveConroy.com
-#10/29/13
+#7/4//14
 #stt.sh
 
+#part 1
 echo "Recording your Speech (Ctrl+C to Transcribe)"
 arecord -D plughw:0,0 -f cd -t wav -d 0 -q -r 16000 | flac - -s -f --best --sample-rate 16000 -o daveconroy.flac;
 
-echo "Converting Speech to Text..."
-wget -q -U "Mozilla/5.0" --post-file daveconroy.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  > stt.txt
-
-echo "You Said:"
-value=`cat stt.txt`
-echo "$value"
-
 #part 2
-#python pitranslate.py -o en -d es -t "$value"
+python text-to-translate.py
+
+
 
